c046b0d413f31ffb05f8d5cd61a222d27f3633b2
Ömür Özkir <oemuer.oezkir@gmail.com>
2018-06-05 10:25:52 +0200

    loads the measured data from api

--- src/web/app.py
+++ src/web/app.py
@@ -3,14 +3,23 @@
 # calc prediction
 # serve prediction + actual values as json
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template
+import requests
+import json
+import os
+
 app = Flask(__name__)
+
+token = os.environ['API_TOKEN']
+measurements = requests.get(
+    url=f"https://habitat-lifesupport.herokuapp.com/measurements?token={token}").json()[:24][::-1]
+print(measurements)
 
 
 @app.route('/', methods=['GET'])
 def root():
     '''renders the index template with json embedded as data attribute'''
-    return render_template('index.html', measurements=[12, 13, 14, 15], predictions=[23, 24])
+    return render_template('index.html', measurements=json.dumps(measurements), predictions=[23, 24])
 
 
 if __name__ == '__main__':
