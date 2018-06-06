0fe8a1482b824f289ba36f3ce2c22f519d20edd0
Ömür Özkir <oemuer.oezkir@gmail.com>
2018-06-06 15:19:39 +0200

    gathers predictions for the next 24h

--- pkg/web/app.py
+++ pkg/web/app.py
@@ -16,8 +16,9 @@ def root():
     '''renders the index template with json embedded as data attribute'''
     measurements = requests.get(
         url=f"https://habitat-lifesupport.herokuapp.com/measurements?token={token}").json()[:72][::-1]
-    last_24h, last_24h_predictions = prepare_data(measurements)
-    return render_template('index.html', measurements=json.dumps(last_24h), predictions=json.dumps(last_24h_predictions))
+    last_24h, last_24h_predictions, next_24h_predictions = prepare_data(
+        measurements)
+    return render_template('index.html', measurements=json.dumps(last_24h), predictions=json.dumps(last_24h_predictions), next_day=json.dumps(next_24h_predictions))
 
 
 def prepare_data(measurements):
@@ -25,12 +26,19 @@ def prepare_data(measurements):
         __file__), '../../models/hum_lin_reg.pkl')
     df = pd.DataFrame(measurements)
     last_24h = df[48:].to_dict('records')
+    last_48h = df[24:].humidity.values
     previous_two_days = df[0:48].humidity.values
     last_24h_predictions = HumidityLinearRegression.predict_day(
         model_location, previous_two_days)
     last_24h_predictions = [
         {'humidity': n, 'measuredAt': last_24h[i]['measuredAt']} for i, n in enumerate(last_24h_predictions)]
-    return last_24h, last_24h_predictions
+    next_24h_predictions = HumidityLinearRegression.predict_day(
+        model_location, last_48h)
+    next_labels = [str(i) for i in pd.date_range(
+        last_24h[-1]['measuredAt'], periods=25, freq='H')[1:]]
+    next_24h_predictions = [
+        {'humidity': n, 'measuredAt': next_labels[i]} for i, n in enumerate(next_24h_predictions)]
+    return last_24h, last_24h_predictions, next_24h_predictions
 
 
 if __name__ == '__main__':
